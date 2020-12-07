d15bf3a5f18d4fdcd04067cb8ea0d404d21b7869
NicholasETerry <nicholas.e.terry@gmail.com>
2020-12-07 11:28:30 -0600

    attempt to update second time

---
+++ RobotsVsDinosaurProject/Dinosaur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaurProject
+{
+    class Dinosaur
+    {
+        /// type
+        string type;
+        /// health
+        int health;
+        /// energy
+        int energy;
+        /// attack power
+        int attackPower;
+        /// 
+        /// instantiate three dinosaur objects and assign the appropriate values to all the objects
+        /// dinosaur objects will be stored in a Herd
+        /// Ability to attack a robot on the battelfield
+        /// dinosaur will loose health points based on attak power of robot
+        public Dinosaur(string dinoType)
+        {
+            this.health = 100;
+            // DEPENDING ON DINOTYPE
+            // ENERGY
+            //ATTACK POWER
+        }
+    }
+}
---
+++ RobotsVsDinosaurProject/Robot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaurProject
+{
+    class Robot
+    {
+        Random newRandom = new Random();
+        
+        /// will contain
+        /// name
+        string name;
+        /// health
+        int health;
+        /// power level
+        int powerLevel;
+        /// weapon with a type ( sword or gun or knife...)
+        string weapon ;
+        /// attack power
+        int attackPower;
+        /// 
+        ///instantiate three robot objects and assing there appropriate values
+        ///robot objects stored in the fleet
+        ///robot can attack dino on the battlefield
+        ///robot will loose health points based on dino attack power
+        public Robot(string name)
+        {
+            this.health = 100;
+            this.powerLevel = newRandom.Next(40, 100);
+            this.weapon = "Plasma Blaster";
+            this.attackPower = 100;
+        }
+    }
+}
